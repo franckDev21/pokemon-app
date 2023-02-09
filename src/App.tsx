adb147a9e4d6e44db1ac8a7485d62b775a6d67c1
franckDev21 <tutodevphp@gmail.com>
2023-02-09 14:07:51 +0100

    add react router dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.scss'
+import { Route } from "react-router";
+import { Routes, BrowserRouter } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <div className="App">
-      <h1 className="text-green-500 text-xl">init</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<>Not fount !</>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Home: FC<{ className?: string }> = ({ className = "" }) => {
+  return <div className="text-black">Home</div>;
+};
+
+export default Home;
